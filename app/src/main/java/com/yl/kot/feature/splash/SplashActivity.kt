789df843a4c0daa9f671449c7812badae9d4e6ea
qiuJuJu <yehongfei1994@foxmail.com>
2019-08-15 18:14:37 +0800

    Splash计时用协程实现

--- app/src/main/java/com/yl/kot/feature/splash/SplashActivity.kt
+++ app/src/main/java/com/yl/kot/feature/splash/SplashActivity.kt
@@ -1,10 +1,13 @@
 package com.yl.kot.feature.splash
 
-import android.os.Handler
 import android.widget.TextView
 import com.yl.kot.Page
 import com.yl.kot.R
 import com.yl.kot.base.BaseActivity
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * Created on 2019/7/27.
@@ -16,11 +19,7 @@ class SplashActivity : BaseActivity() {
 
     private lateinit var mTvSkip: TextView
 
-    private val mHandler: Handler by lazy {
-        Handler()
-    }
-
-    private val mRunnable: Runnable = Runnable { jump2MainView() }
+    private lateinit var mTimerJob: Job
 
     override fun getLayoutId(): Int {
         return R.layout.activity_splash
@@ -35,25 +34,27 @@ class SplashActivity : BaseActivity() {
 
     override fun onResume() {
         super.onResume()
-        mHandler.removeCallbacks(mRunnable)
-        mHandler.postDelayed(mRunnable, AUTO_JUMP_DELAY)
+        mTimerJob = MainScope().launch {
+            delay(AUTO_JUMP_DELAY)
+            jump2MainView()
+        }
     }
 
     override fun onPause() {
         super.onPause()
-        mHandler.removeCallbacks(mRunnable)
+        mTimerJob.cancel()
     }
 
     /**
      * 跳转到主界面
      */
     private fun jump2MainView() {
-        mHandler.removeCallbacks(mRunnable)
+        mTimerJob.cancel()
         Page.toHome()
         finish()
     }
 
     companion object {
-        private const val AUTO_JUMP_DELAY: Long = 3000L
+        private const val AUTO_JUMP_DELAY: Long = 3_000
     }
 }
